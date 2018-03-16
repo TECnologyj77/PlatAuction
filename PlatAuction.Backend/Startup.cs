04aa8e957644339c11f1f7478436bfdf30663c58
Thomas Carney <tcarney@nepgroup.com>
2018-03-16 12:28:16 -0400

    Add project files.

---
+++ PlatAuction.Backend/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(PlatAuction.Backend.Startup))]
+
+namespace PlatAuction.Backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
