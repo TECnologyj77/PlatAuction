74758c78f78fbba9c736245272906b0bd42dcf75
TECnologyj77 <Thomas.Carney007@gmail.com>
2018-03-16 15:18:32 -0400

    added button to projects

---
+++ PlatAuction/PlatAuction/Auctions.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.WindowsAzure.MobileServices;
+using Xamarin.Forms;
+
+namespace PlatAuction
+{
+    public partial class Auctions : ContentPage
+    {
+        public Auctions()
+        {
+            InitializeComponent();
+        }
+
+        public async void Button_Clicked(object sender, EventArgs e){
+            MobileServiceClient client = new MobileServiceClient("http://TCarney-640G2.nepbroadcasting.com/PlatAuction/");
+
+            var todoItems = await client.GetTable<ToDoItem>().ReadAsync();
+            message.Text = todoItems.First().Text;
+        }
+    }
+}
---
+++ PlatAuction/PlatAuction/ToDoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace PlatAuction
+{
+    public class ToDoItem
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public bool Complete { get; set; }
+
+        public ToDoItem()
+        {
+        }
+    }
+}
