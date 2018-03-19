e03ce0abe040dfeafeebf5ab6412379d9a59add8
TECnologyj77 <thomas.carney007@gmail.com>
2018-03-19 14:42:57 -0400

    commit

--- PlatAuction/PlatAuction.iOS/Main.cs
+++ PlatAuction/PlatAuction.iOS/Main.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Foundation;
+using Microsoft.WindowsAzure.MobileServices;
 using UIKit;
 
 namespace PlatAuction.iOS
@@ -14,7 +15,15 @@ namespace PlatAuction.iOS
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
+            try
+            {
+                CurrentPlatform.Init();
+                UIApplication.Main(args, null, "AppDelegate");
+            }
+            catch(Exception e)
+            {
+                throw (e);
+            }
         }
     }
 }
